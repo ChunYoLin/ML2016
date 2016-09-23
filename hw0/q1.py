0e05be4ca7486d0b875f52b22540a38377e0fa06
ChunYo <r04922108@csie.ntu.edu.tw>
2016-09-23 13:54:28 +0800

    HW0

---
+++ hw0/q1.py
@@ -0,0 +1,12 @@
+import numpy as np
+import sys
+data = np.loadtxt(str(sys.argv[2]))
+out = open('ans1.txt','w')
+sorted_data = np.sort(data[int(sys.argv[1])])
+length = data[int(sys.argv[1])].size
+for i in range(length):
+    out.write(str(sorted_data[i]))
+    if i != length-1:
+        out.write(',')
+out.close()
+
