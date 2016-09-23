561acaf12dc01498f412bf030f7dc0b8997fa1e9
ChunYo <r04922108@csie.ntu.edu.tw>
2016-09-23 14:20:49 +0800

    HW0

--- hw0/q1.py
+++ hw0/q1.py
@@ -2,8 +2,8 @@ import numpy as np
 import sys
 data = np.loadtxt(str(sys.argv[2]))
 out = open('ans1.txt','w')
-sorted_data = np.sort(data[int(sys.argv[1])])
-length = data[int(sys.argv[1])].size
+sorted_data = data[data[:,int(sys.argv[1])].argsort()][:,int(sys.argv[1])]
+length = sorted_data.size
 for i in range(length):
     out.write(str(sorted_data[i]))
     if i != length-1:
