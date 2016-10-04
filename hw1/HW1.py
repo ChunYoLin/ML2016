8df25c6916014bb74e34fa93b75dbbf64bcc2a1a
ChunYoLin <r04922108@csie.ntu.edu.tw>
2016-10-04 23:49:47 +0800

    train data preparation

--- hw1/HW1.py
+++ hw1/HW1.py
@@ -3,18 +3,21 @@ import numpy as np
 import pandas as pd
 import re
 from datetime import datetime, date
-columns = ["date", "station", "item"] + [str(i) for i in range(24)]
-raw_data = pd.read_csv('./data/train.csv', names = columns)
 
-raw_data = raw_data.drop("station", 1).drop(raw_data.index[0]).set_index(["date"])
-train_data = pd.DataFrame()
-train_file = open('./data/train.csv')
+hash_table = {
+    "AMB_TEMP":0, "CH4":1, "CO":2, "NMHC":3, "NO":4, "NO2":5, 
+    "NOx":6, "O3":7, "PM10":8, "PM2.5":9, "RAINFALL":10, 
+    "RH":11, "SO2":12, "THC":13, "WD_HR":14, "WIND_DIREC":15,
+    "WIND_SPEED":16, "WS_HR":17
+    }
+inv_hash_table = {v: k for k, v in hash_table.items()}
+train_data = [[] for i in range(18)]
+print train_data
 
-for idx, line in enumerate(train_file):
+raw_file = open('./data/train.csv')
+for idx,row in enumerate(raw_file):
     if idx > 0:
-        raw_list = re.sub('\r\n', '', line).split(',')
-        date = datetime.strptime(raw_list[0], "%Y/%m/%d")
-        item = raw_list[2]
-        data = raw_list[3:]
-        train_data[date, item] = data
-print train_data[datetime(2014,1,1), "PM2.5"][23]
+        data = re.sub('\r|\n', '', row).split(',')
+        train_data[hash_table[data[2]]]+=data[3:]
+
+
