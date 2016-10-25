b566f0f4fef5a2a4e3048065643f5df9d0c0144e
ChunYoLin <r04922108@csie.ntu.edu.tw>
2016-10-26 01:04:38 +0800

    .

--- hw2/test.py
+++ hw2/test.py
@@ -1,6 +1,7 @@
 import numpy as np
 import re
-test_data = open('./spam_data/spam_test.csv', 'r')
+import sys
+test_data = open(sys.argv[2], 'r')
 x = []
 for row in test_data:
     row_l = re.sub('\n|\r', '', row).split(',')
@@ -9,13 +10,13 @@ x = np.asarray(x, dtype = np.float32)
 bias = np.ones(shape = (x.shape[0], 1))
 x = np.concatenate((bias, x), axis = 1)
 w = np.zeros(shape = x.shape[1])
-weight_file = open('./spam.weights', 'r')
+weight_file = open(sys.argv[1], 'r')
 weight = weight_file.read().split(',')
 weight.remove('')
 for idx, value in enumerate(weight):
     w[idx] = float(value)
 y_ = 1. / (1. + np.exp(((-1.) * np.dot(x, w))))
-pred = open('./pred.csv', 'w')
+pred = open(sys.argv[3], 'w')
 pred.write('id,label\n')
 for i in range(y_.shape[0]):
     if y_[i] > 0.5:
--- hw2/train.py
+++ hw2/train.py
@@ -1,7 +1,7 @@
 import numpy as np
 import re
-
-train_data = open('./spam_data/spam_train.csv', 'r')
+import sys
+train_data = open(sys.argv[1], 'r')
 feature = []
 x = []
 y = []
@@ -14,26 +14,42 @@ bias = np.ones(shape = (x.shape[0], 1))
 x = np.concatenate((bias, x), axis = 1)
 w = np.zeros(shape = x.shape[1])
 y = np.asarray(y, dtype = np.float32)
-m = x.shape[0]
-Learning_rate = 0.5 
+k = x.shape[0]
+Learning_rate = 0.005
 gw_his = 0.
-k = 0
+t = 0
+BETA1 = 0.9
+BETA2 = 0.999
+E = 10**(-8)
+m = np.zeros_like(w)
+v = np.zeros_like(w)
+
 while True:
+    t += 1
     y_ = 1. / (1. + np.exp(((-1.) * np.dot(x, w))))
+    acc = 0.
     for i in range(y_.shape[0]):
+        if (y_[i] > 0.5 and y[i] == 1) or (y_[i] <= 0.5 and y[i] == 0):
+            acc += 1
         if y_[i] == 1:
             y_[i] -= 0.000000000001
-    L = (1. / m) * np.sum((-y) * np.log(y_) - (1. - y) * np.log(1. - y_))
+    L = (1. / k) * np.sum((-y) * np.log(y_) - (1. - y) * np.log(1. - y_))
     gw = -np.dot(x.transpose(), (y - y_))
-    gw_his += gw**2
     gw_mean = np.mean(np.abs(gw))
-    print 'iter', k
+    print 'iter', t
     print 'Learning_rate', Learning_rate
     print 'cross entrophy', L
     print 'mean of gradient', gw_mean
-    w -= (Learning_rate / np.sum(gw_his, axis = 0)**0.5) * gw 
-    if gw_mean < 0.04:
-        w_file_name = './spam.weights'
+    print 'acc', acc / y_.shape[0]
+    m = BETA1 * m + (1 - BETA1) * gw
+    v = BETA2 * v + (1 - BETA2) * gw**2
+    m_hat = m / (1 - BETA1**t)
+    v_hat = v / (1 - BETA2**t)
+    w -= Learning_rate * m_hat / (v_hat**0.5 + E) 
+    #  gw_mean = np.mean(np.abs(gw))
+    #  w -= (Learning_rate / np.sum(gw_his, axis = 0)**0.5) * gw 
+    if t == 4000:
+        w_file_name = sys.argv[2]
         w_file = open(w_file_name, 'w')
         for i in range(len(w)):
             w_file.write(str(w[i]))
