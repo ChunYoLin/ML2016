680c077057bcaf58fd3e69244aab6b98b555edab
ChunYoLin <r04922108@csie.ntu.edu.tw>
2016-11-09 01:57:14 +0800

    .

--- hw3/CNN.py
+++ hw3/CNN.py
@@ -53,13 +53,13 @@ h_drop1 = tf.nn.dropout(h_pool1, keep_prob)
 #  h_conv2 = tf.nn.relu(conv2d(norm1, W_conv2) + b_conv2)
 #  h_pool2 = tf.nn.max_pool(h_conv2, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'SAME')
 #  fully connected layer 1
-W_fc1 = tf.Variable(tf.truncated_normal(shape = [16 * 16 * 128, 1024], stddev = 0.04))
+W_fc1 = tf.Variable(tf.truncated_normal(shape = [16 * 16 * 128, 1024], stddev = 1 / 1024.))
 b_fc1 = tf.Variable(tf.constant(value = 0.1, shape = [1024]))
 h_drop1_flat = tf.reshape(h_drop1, [-1, 16 * 16 * 128])
 h_fc1 = tf.nn.relu(tf.matmul(h_drop1_flat, W_fc1) + b_fc1)
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 #  output layer
-W_fc2 = tf.Variable(tf.truncated_normal(shape = [1024, 10], stddev = 1 / 1024.))
+W_fc2 = tf.Variable(tf.random_normal(shape = [1024, 10], stddev = 0.01))
 b_fc2 = tf.Variable(tf.constant(value = 0., shape = [10]))
 y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 
